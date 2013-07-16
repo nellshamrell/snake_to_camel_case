a33e847081ed7fe046bd5346229d82fae1f16465
Nell Shamrell <nshamrell@bluebox.net>
2013-07-15 19:14:02 -0700

    upcase first word character of string

--- lib/case_converter.rb
+++ lib/case_converter.rb
@@ -1,3 +1,5 @@
 class CaseConverter
-
+  def snake_to_camel(string)
+    string.gsub(/^\w/) {|char| char.upcase}
+  end
 end
--- spec/case_converter_spec.rb
+++ spec/case_converter_spec.rb
@@ -2,4 +2,12 @@ require_relative '../spec/spec_helper'
 
 describe CaseConverter do
 
+  before(:each) do
+    @case_converter = CaseConverter.new
+  end
+
+  it "capitalizes the first character of a string" do
+    @case_converter.snake_to_camel("method").should == "Method"
+  end
+
 end
