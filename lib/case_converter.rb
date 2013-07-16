fd35f57426707b0c9752fd620145cc9081c57720
Nell Shamrell <nshamrell@bluebox.net>
2013-07-15 19:41:11 -0700

    moving separate gsub methods to be private methods

--- lib/case_converter.rb
+++ lib/case_converter.rb
@@ -1,4 +1,10 @@
 class CaseConverter
+  def snake_to_camel(string)
+    remove_underscores(upcase_characters(string))
+  end
+
+  private
+
   def upcase_characters(string)
     string.gsub(/(^[^a-z]?[a-z])|(_[a-z])/) {|char| char.upcase}
   end
@@ -7,7 +13,4 @@ class CaseConverter
     string.gsub(/_/, '')
   end
 
-  def snake_to_camel(string)
-    remove_underscores(upcase_characters(string))
-  end
 end
--- spec/case_converter_spec.rb
+++ spec/case_converter_spec.rb
@@ -6,22 +6,6 @@ describe CaseConverter do
     @case_converter = CaseConverter.new
   end
 
-  it "capitalizes the first character of a string" do
-    @case_converter.upcase_characters("method").should == "Method"
-  end
-
-  it "capitalizes the first letter of a string" do
-    @case_converter.upcase_characters("_method").should == "_Method"
-  end
-
-  it "capitalizes any character that follows an underscore" do
-    @case_converter.upcase_characters("some_method_name").should == "Some_Method_Name"
-  end
-
-  it "removes underscores throughout the string" do
-    @case_converter.remove_underscores("Some_Method_Name").should == "SomeMethodName"
-  end
-
   it "converts snake_case to CamelCase" do
     @case_converter.snake_to_camel("some_method_name").should == "SomeMethodName"
   end
