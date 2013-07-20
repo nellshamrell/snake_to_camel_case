ed3285ac2ef12b196656c724066a497d9663a605
Nell Shamrell <nshamrell@bluebox.net>
2013-07-19 20:02:55 -0700

    fixing regex bug

--- lib/case_converter.rb
+++ lib/case_converter.rb
@@ -1,6 +1,6 @@
 class CaseConverter
   def upcase_characters(string)
-    string.gsub(/(^[a-z])|(?<=_)[a-z]/) {|char| char.upcase}
+    string.gsub(/(^[^a-z]?[a-z])|(?<=_)[a-z]/) {|char| char.upcase}
   end
 
   def remove_underscores(string)
